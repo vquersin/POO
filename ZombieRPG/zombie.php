28f6fbc6c30f9a741a56ac94fcbb9d31f39e3ef0
vquersin <quersin.valentin@gmail.com>
2023-05-25 16:24:39 +0200

    Done

--- ZombieRPG/zombie.php
+++ ZombieRPG/zombie.php
@@ -1,21 +1,21 @@
 <?php
-    class Zombie extends Personnage{
-        private $level;
-        private static $vitesse;
+    class Zombie extends Personnage {
+    private $level;
+    private $vitesse;
 
-        public function __construct($nom,$classe,$attaque,$pv,$forceDuBien,$level,$vitesse){
-            parent::__construct($nom,$classe,$attaque,$pv,$forceDuBien);
-            $this->level = $level;
-            self::$vitesse = $vitesse;            
-        }
-        public function getVitesse(){return self::$vitesse;}
-
-        public function __toString(){
-
-            $txt = "";
-            $txt .= parent::__toString();
-            $txt .= "Level : " . $this->level . "  et une vitesse de ". self::$vitesse ."<br>";
-            return $txt;  
-        }
+    public function __construct($nom, $classe, $attaque, $pv, $forceDuBien, $level, $vitesse) {
+        parent::__construct($nom, $classe, $attaque, $pv, $forceDuBien);
+        $this->level = $level;
+        $this->vitesse = $vitesse;            
     }
+
+    public function __toString() {
+        $txt = "";
+        $txt .= parent::__toString();
+        $txt .= "Level: " . $this->level . " et une vitesse de " . $this->vitesse . "<br>";
+        return $txt;  
+    }
+}
+
+
  ?>
